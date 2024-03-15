82f01aceab896b10048b350c62d90e6e82639419
Love Little Liu Only <98511891+yeweilin666@users.noreply.github.com>
2024-03-15 13:00:54 +0800

    Update 美团.js

--- 美团.js
+++ 美团.js
@@ -1,26 +1,4 @@
-!(() => {
-    "use strict";
-    const $toString = Function.toString;
-    const myFunction_toString_symbol = Symbol('('.concat('', ')_', (Math.random() + '').toString(36)));
-    const myToString = function () {
-        return typeof this == 'function' && this[myFunction_toString_symbol] || $toString.call(this);
-    };
 
-    function set_native(func, key, value) {
-        Object.defineProperty(func, key, {
-            "enumerable": false,
-            "configurable": true,
-            "writable": true,
-            "value": value
-        })
-    };
-    delete Function.prototype['toString']; //删除原型链上的toString
-    set_native(Function.prototype, "toString", myToString); //自己定义个getter方法
-    set_native(Function.prototype.toString, myFunction_toString_symbol, "function toString() { [native code] }"); //套个娃 保护一下我们定义的toString 否则就暴露了
-    this.func_set_natvie = (func) => {
-        set_native(func, myFunction_toString_symbol, `function ${myFunction_toString_symbol, func.name || ''}() { [native code] }`);
-    }; //导出函数到globalThis
-}).call(this);
 cl=console.log
 appkey = ""
 const XMLHttpRequest = require('xhr2');
@@ -211,10 +189,6 @@ window.sessionStorage.setItem = function setItem(key, value) {
 };
 window.fetchHooked = true
 window.wDomains =[
-    "appsec-mobile.sec.test.sankuai.com",
-    "appsec-mobile.meituan.com",
-    "msp.meituan.com",
-    "pikachu.mykeeta.com"
 ]
 window.name = ''
 window.indexedDB = {}
